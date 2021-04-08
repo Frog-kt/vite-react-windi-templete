8dc095fe9e01c3a7579334d23001bb3c382496f9
Frog-kt <kasuo.takumi@gmail.com>
2021-04-08 13:37:40 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className="App text-center">
+      <h1 className="text-gray-700 text-5xl">Welcome!</h1>
+      <p className="text-gray-700">This project is Vite, React, TypeScript, WindiCSS Templete</p>
+      <p className="text-yellow-600 text-xl">Lets Enjoy!</p>
+    </div>
+  )
+}
+
+export default App
